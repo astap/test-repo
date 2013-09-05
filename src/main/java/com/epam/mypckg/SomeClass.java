baae602549fa6fadeddb44da142ce9bfdd512fd3
Atsiom_Astapenka <artsiom_astapenka@epam.com>
2013-09-05 12:16:53 +0300

    new commit with true test

---
+++ src/main/java/com/epam/mypckg/SomeClass.java
@@ -0,0 +1,7 @@
+package com.epam.mypckg;
+
+public class SomeClass {
+    public boolean someMethod() {
+        return false;
+    }
+}
---
+++ src/main/tests/com/epam/mypckg/SomeClassTest.java
@@ -0,0 +1,18 @@
+package com.epam.mypckg;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SomeClassTest {
+    private SomeClass someClass;
+    @Before
+    public void setUp() throws Exception {
+        someClass = new SomeClass();
+    }
+
+    @Test
+    public void testSomeMethod() throws Exception {
+         Assert.assertFalse(someClass.someMethod());
+    }
+}
