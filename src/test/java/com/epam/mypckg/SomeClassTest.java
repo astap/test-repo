8681a76e6379aff7ab1ef566b9dbbcf60520f3d6
Atsiom_Astapenka <artsiom_astapenka@epam.com>
2013-09-09 12:33:44 +0300

    failure test

---
+++ src/test/java/com/epam/mypckg/SomeClassTest.java
@@ -0,0 +1,19 @@
+package com.epam.mypckg;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SomeClassTest {
+    private SomeClass someClass;
+
+    @Before
+    public void setUp() throws Exception {
+        someClass = new SomeClass();
+    }
+
+    @Test
+    public void testFalseMethod() throws Exception {
+        Assert.assertFalse(!someClass.getFalse());
+    }
+}
